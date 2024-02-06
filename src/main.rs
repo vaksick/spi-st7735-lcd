abf008269e8516bb6e829a9aefb0f5226f011e9e
vaksick <vaksick@gmail.com>
2024-02-06 17:39:32 +0200

    clk

--- src/main.rs
+++ src/main.rs
@@ -14,21 +14,19 @@ use stm32f4xx_hal::{
     spi,
     gpio::PinState,
 };
-// use hal::{gpio::NoPin, pac, prelude::*};
-// use display_interface_spi::SPIInterfaceNoCS;
+
 use st7735_lcd;
 use st7735_lcd::Orientation;
+
+use embedded_graphics::image::{Image, ImageRaw, ImageRawLE};
+use embedded_graphics::prelude::*;
+use embedded_graphics::pixelcolor::Rgb565;
 
 /// SPI mode
 pub const MODE: spi::Mode = spi::Mode {
     phase: spi::Phase::CaptureOnFirstTransition,
     polarity: spi::Polarity::IdleLow,
 };
-
-
-use embedded_graphics::image::{Image, ImageRaw, ImageRawLE};
-use embedded_graphics::prelude::*;
-use embedded_graphics::pixelcolor::Rgb565;
 
 #[entry]
 fn main() -> ! {
@@ -37,11 +35,9 @@ fn main() -> ! {
         pac::Peripherals::take(),
         cortex_m::peripheral::Peripherals::take(),
     ) {
-        // create SPI interface
-        // let spi = spim::Spim::new(p.SPIM0, pins, spim::Frequency::M8, spim::MODE_3, 122);
 
         let rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.use_hse(25.MHz()).sysclk(48.MHz()).freeze();
+        let clocks = rcc.cfgr.sysclk(84.MHz()).freeze();
         let mut delay = dp.TIM1.delay_us(&clocks);
         let gpioa = dp.GPIOA.split();
         let gpiob = dp.GPIOB.split();
@@ -49,12 +45,12 @@ fn main() -> ! {
         let sck = gpioa.pa5.into_alternate::<5>();
         // let miso = gpioa.pa6.into_alternate();
         let mosi = gpioa.pa7.into_alternate();
-        let rst = gpiob.pb0.into_push_pull_output_in_state(PinState::High);
-        let dc = gpiob.pb1.into_push_pull_output_in_state(PinState::High);
+        let rst = gpiob.pb0.into_push_pull_output();
+        let dc = gpiob.pb1.into_push_pull_output();
         let spi = dp.SPI1.spi_bidi(
             (sck, mosi),
             MODE,
-            3.MHz(),
+            25.MHz(),
             &clocks,
         );
         delay.delay_ms(100);
@@ -80,8 +76,3 @@ fn main() -> ! {
 
     loop {}
 }
-
-// #[exception]
-// fn HardFault(ef: &ExceptionFrame) -> ! {
-//     panic!("{:#?}", ef);
-// }
